0e8e9e4a54d6dfee2324a280d14d934624596345
tanachotkub <tanachot2004@gmail.com>
2026-04-29 22:08:54 +0700

    feat: configure CORS to use FRONTEND_URL and add it to docker-compose

--- backend/middlewares/cors.go
+++ backend/middlewares/cors.go
@@ -1,13 +1,15 @@
 package middlewares
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func SetupCORS(app *fiber.App) {
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "*",
+		AllowOrigins: os.Getenv("FRONTEND_URL"),
 		AllowMethods: "GET,POST,OPTIONS,DELETE,PUT",
 		AllowHeaders: "Content-Type, Authorization",
 	}))
